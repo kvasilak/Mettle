d7c9226fbd04058b7ab1f3afec291c46e5607464
keith vasilakes <my427v8@hotmail.com>
2013-12-01 12:00:49 -0600

    Fixed GPL preamble and added GPL text

--- Charter/TagChart.cs
+++ Charter/TagChart.cs
@@ -6,11 +6,6 @@
 //the Free Software Foundation, either version 3 of the License, or
 //(at your option) any later version.
 //
-//In addition any and all modifications made to this program belong to the origional copyright holder.
-//This includes existing code and new code that is designed to work within the framework or expands this framework.
-//All such changes shall be published as a branch to the origional code within 6 months of creation.
-//All New source files shall contain this notice.
-//
 //This program is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
